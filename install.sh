fe62b3bc502ba7722f7d6430c09bf5fa6501d5f9
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2026-04-28 19:51:06 +0900

    update install

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-if [[ $1 =~ (adopt|diff) ]]; then
-        MODE="--adopt"
-fi
-
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 for dir in "$BASE_DIR"/*/; do
         dir=${dir%/}
         dir=${dir##*/}
 
-        read -n 1 -p "stow -v $MODE $dir? [y/n] " ans
+        read -n 1 -p "stow -v -t \$HOME $@ $dir? [y/n] " ans
         echo
         if [[ "$ans" =~ ^[yY] ]]; then
-                stow -v -t "$HOME" ${MODE:+$MODE} "$dir"
+                stow -v -t "$HOME" "$@" "$dir"
         else
                 echo skipping "$dir"
         fi
