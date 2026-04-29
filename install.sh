45a8a433a432e23aeed0591c4d2d6b605501fe9b
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2026-04-30 00:11:00 +0900

    update install

--- install.sh
+++ install.sh
@@ -2,14 +2,28 @@
 
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 for dir in "$BASE_DIR"/*/; do
-        dir=${dir%/}
-        dir=${dir##*/}
+	dir=${dir%/}
+	dir=${dir##*/}
 
-        read -n 1 -p "stow -v -t \$HOME $@ $dir? [y/n] " ans
-        echo
-        if [[ "$ans" =~ ^[yY] ]]; then
-                stow -v -t "$HOME" "$@" "$dir"
-        else
-                echo skipping "$dir"
-        fi
+	read -r -n 1 -p "stow -v -t \$HOME $* $dir? [y/n] " ans
+	echo
+	if [[ "$ans" =~ ^[yY] ]]; then
+		stow -v -t "$HOME" "$@" "$dir"
+
+		case "$dir" in
+		bash)
+			line='[ -f ~/.bashrc.local ] && . ~/.bashrc.local'
+			if ! grep -Fq "$line" ~/.bashrc; then
+				cat <<-EOF >>~/.bashrc
+					# Load local bashrc
+					$line
+				EOF
+
+				echo Added line in bashrc
+			fi
+			;;
+		esac
+	else
+		echo skipping "$dir"
+	fi
 done
